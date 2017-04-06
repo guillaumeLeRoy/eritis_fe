6787e367d48ebf924d1fcb83b5743e0839745577
guillaumeLeRoy <gleroy78@gmail.com>
2017-04-06 08:53:00 +0100

    Add API call to contact iritis (#2)

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by guillaume on 03/04/2017.
+ */
+export const environment = {
+  production: false,
+  BACKEND_BASE_URL: "http://localhost:8080/api",
+  firebase_apiKey: "AIzaSyAAszel5d8YQuuGyZ65lX89zYb3V6oqoyA",
+  firebase_authDomain: "eritis-be-glr.firebaseapp.com",
+  firebase_databaseURL: "https://eritis-be-glr.firebaseio.com",
+};
