e82a023afac25c9af18bc58c61076f989cafc36e
jordhan <jordhan.madec@gmail.com>
2017-04-18 22:13:06 +0200

    Resolved confilcts

--- src/app/meeting/pre-meeting.component.ts
+++ src/app/meeting/pre-meeting.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnInit, Input, EventEmitter, Output} from '@angular/core';
-import {FormGroup, Validators, FormBuilder} from "@angular/forms";
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import {CoachCoacheeService} from "../service/CoachCoacheeService";
-import {MeetingReview} from "../model/MeetingReview";
+import {
+  MEETING_REVIEW_TYPE_SESSION_CONTEXT, MEETING_REVIEW_TYPE_SESSION_GOAL,
+  MeetingReview
+} from "../model/MeetingReview";
 
 @Component({
   selector: 'er-pre-meeting',
@@ -13,76 +15,128 @@ export class PreMeetingComponent implements OnInit {
   @Input()
   meetingId: string;
 
-  private form: FormGroup;
-
-  private displayErrorPostingReview = false;
+  @Output()
+  meetingGoal = new EventEmitter<string>();
+  private uiMeetingGoal: string;
 
   @Output()
-  reviewPosted = new EventEmitter<MeetingReview>();
+  meetingContext = new EventEmitter<string>();
+  private uiMeetingContext: string;
 
-  private meetingReviewContexts: MeetingReview[];
-
-  constructor(private formBuilder: FormBuilder, private coachService: CoachCoacheeService) {
+  constructor(private coachService: CoachCoacheeService) {
   }
 
   ngOnInit() {
+<<<<<<< HEAD
     this.form = this.formBuilder.group({
       context: ['', [Validators.required]],
       objectif: ['', []]
     });
+=======
+    console.log("PreMeetingComponent onInit");
+    this.getAllMeetingReviews();
+>>>>>>> origin/feature/dashboard_coachee
 
-    this.getAllMeetingReviewContexts();
   }
 
+  /* Get form API all reviews for the given meeting */
+  getAllMeetingReviews() {
+    console.log("getAllMeetingReviews, meetingId : ", this.meetingId);
 
+<<<<<<< HEAD
   getAllMeetingReviewContexts() {
     console.log("getAllMeetingReviewContexts, meetingId : ", this.meetingId);
 
     this.coachService.getMeetingContext(this.meetingId).subscribe(
+=======
+    this.coachService.getMeetingReviews(this.meetingId).subscribe(
+>>>>>>> origin/feature/dashboard_coachee
       (reviews: MeetingReview[]) => {
-        console.log("getAllMeetingReviewContexts, got reviews : ", reviews);
-        //emit event
-        //this.reviewPosted.emit(review);
-        this.meetingReviewContexts = reviews;
+        console.log("getAllMeetingReviews, got reviews : ", reviews);
+
+        if (reviews != null) {
+          //search for correct type
+          for (let review of reviews) {
+            if (review.type == MEETING_REVIEW_TYPE_SESSION_GOAL) {
+              this.updateGoalValue(review.comment);
+            } else if (review.type == MEETING_REVIEW_TYPE_SESSION_CONTEXT) {
+              this.updateContextValue(review.comment);
+            }
+          }
+        }
       },
       (error) => {
-        console.log('getAllMeetingReviewContexts error', error);
+        console.log('getAllMeetingReviews error', error);
         //this.displayErrorPostingReview = true;
       });
   }
 
+<<<<<<< HEAD
   submitMeetingContextForm() {
     console.log("submitMeetingContextForm form : ", this.form.value);
+=======
+  onGoalValueChanged(event) {
+    let goal = event.target.value;
+    console.log('onGoalValueChanged res', goal);
+>>>>>>> origin/feature/dashboard_coachee
 
-    this.coachService.addAContextToMeeting(this.meetingId, this.form.value.context).subscribe(
-      (review: MeetingReview) => {
-        console.log("submitMeetingContextForm, get review : ", review);
-        //emit event
-        this.reviewPosted.emit(review);
-      },
-      (error) => {
-        console.log('submitMeetingContextForm error', error);
-        this.displayErrorPostingReview = true;
-      }
-    );
+    this.updateGoalValue(goal);
   }
 
+  onContextValueChanged(event) {
+    let context = event.target.value;
+    console.log('onContextValueChanged res', context);
 
+<<<<<<< HEAD
   removeMeetingContext(reviewId) {
     console.log("removeMeetingContext reviewId : ", reviewId);
-
-    this.coachService.removeReview(reviewId).subscribe(
-      (res) => {
-        console.log("removeMeetingContext, done : ", res);
-        //emit event
-        //this.reviewPosted.emit(review);
-      },
-      (error) => {
-        console.log('removeMeetingContext error', error);
-        //this.displayErrorPostingReview = true;
-      }
-    );
-
+=======
+    this.updateContextValue(context);
   }
+>>>>>>> origin/feature/dashboard_coachee
+
+  private updateGoalValue(goal: string) {
+    this.uiMeetingGoal = goal;
+    this.meetingGoal.emit(this.uiMeetingGoal);
+  }
+
+  private updateContextValue(context: string) {
+    this.uiMeetingContext = context;
+    this.meetingContext.emit(this.uiMeetingContext);
+  }
+
+  // submitMeetingContextForm() {
+  //   console.log("submitMeetingContextForm form : ", this.form.value)
+  //
+  //   this.coachService.addAContextForMeeting(this.meetingId, this.form.value.context).subscribe(
+  //     (review: MeetingReview) => {
+  //       console.log("submitMeetingContextForm, get review : ", review);
+  //       //emit event
+  //       this.reviewPosted.emit(review);
+  //     },
+  //     (error) => {
+  //       console.log('submitMeetingContextForm error', error);
+  //       this.displayErrorPostingReview = true;
+  //     }
+  //   );
+  // }
+
+
+  // removeMeetingContext(reviewId) {
+  //   console.log("removeMeetingContext reviewId : ", reviewId)
+  //
+  //   this.coachService.removeReview(reviewId).subscribe(
+  //     (res) => {
+  //       console.log("removeMeetingContext, done : ", res);
+  //       //emit event
+  //       //this.reviewPosted.emit(review);
+  //     },
+  //     (error) => {
+  //       console.log('removeMeetingContext error', error);
+  //       //this.displayErrorPostingReview = true;
+  //     }
+  //   );
+  //
+  // }
 
 }
