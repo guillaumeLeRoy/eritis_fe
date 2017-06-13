8779fb375d84c1fb2261a5eee3c2e878c93aa842
jordhan <jordhan.madec@gmail.com>
2017-06-13 15:07:42 +0200

    Add coachee meetings historic for rh tickets

--- src/app/meeting/meeting-list/rh/meeting-item-rh.component.ts
+++ src/app/meeting/meeting-list/rh/meeting-item-rh.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, Input, OnInit} from "@angular/core";
+import {AfterViewInit, ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output} from "@angular/core";
 import {Coachee} from "../../../model/Coachee";
 import {PotentialCoachee} from "../../../model/PotentialCoachee";
 import {CoachCoacheeService} from "../../../service/CoachCoacheeService";
@@ -6,9 +6,15 @@ import {AuthService} from "../../../service/auth.service";
 import {ApiUser} from "../../../model/apiUser";
 import {Rh} from "../../../model/Rh";
 import {CoacheeObjective} from "../../../model/CoacheeObjective";
+import {Meeting} from "../../../model/meeting";
+import {Observable} from "rxjs/Observable";
+import {MeetingsService} from "../../../service/meetings.service";
+import {RhUsageRate} from "app/model/UsageRate";
+import {MeetingReview} from "../../../model/MeetingReview";
 
 
 declare var $: any;
+declare var Materialize: any;
 
 @Component({
   selector: 'rb-meeting-item-rh',
@@ -23,9 +29,19 @@ export class MeetingItemRhComponent implements OnInit, AfterViewInit {
   @Input()
   potentialCoachee: PotentialCoachee;
 
+  @Output()
+  objectiveChanged = new EventEmitter<string>();
+
   months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
 
   private loading: boolean;
+  private showDetails = false;
+
+  private meetings: Observable<Meeting[]>;
+  private hasBookedMeeting = false;
+  private goals = {};
+
+  private coacheeUsageRate: Observable<RhUsageRate>;
 
   /**
    * Used in Objective modal.
@@ -34,15 +50,14 @@ export class MeetingItemRhComponent implements OnInit, AfterViewInit {
 
   // private coacheeUsageRate: Observable<RhUsageRate>;
 
-  constructor(private authService: AuthService, private coachCoacheeService: CoachCoacheeService) {
+  constructor(private authService: AuthService, private meetingsService: MeetingsService, private coachCoacheeService: CoachCoacheeService, private cd: ChangeDetectorRef) {
   }
 
   ngOnInit(): void {
     console.log('ngOnInit');
 
-    // if (this.coachee) {
-    //   this.getUsageRate(this.coachee.id);
-    // }
+    this.getUsageRate(this.coachee.id);
+    this.getAllMeetingsForCoachee(this.coachee.id);
   }
 
   ngAfterViewInit(): void {
@@ -69,11 +84,65 @@ export class MeetingItemRhComponent implements OnInit, AfterViewInit {
     return (new Date(date)).getDate() + ' ' + this.months[(new Date(date)).getMonth()] + ' ' + (new Date(date)).getFullYear();
   }
 
+  toggleShowDetails() {
+    this.showDetails = this.showDetails ? false : true;
+  }
+
+  private getAllMeetingsForCoachee(coacheeId: string) {
+    this.loading = true;
+
+    this.meetingsService.getAllMeetingsForCoacheeId(coacheeId).subscribe(
+      (meetings: Meeting[]) => {
+        console.log('got meetings for coachee', meetings);
+        let bookedMeetings: Meeting[] = [];
+
+        for (let meeting of meetings) {
+          if (meeting.agreed_date != null) {
+            bookedMeetings.push(meeting);
+            this.hasBookedMeeting = true;
+
+            this.getGoal(meeting.id);
+          }
+        }
+
+        this.meetings = Observable.of(bookedMeetings);
+        this.cd.detectChanges();
+        this.loading = false;
+      }
+    );
+  }
+
+  private getGoal(meetingId: string) {
+    return this.meetingsService.getMeetingGoal(meetingId).subscribe(
+      (reviews: MeetingReview[]) => {
+        console.log("getMeetingGoal, got goal : ", reviews);
+        if (reviews != null)
+          this.goals[meetingId] = reviews[0].comment;
+        else
+          this.goals[meetingId] = 'n/a';
+      },
+      (error) => {
+        console.log('getMeetingGoal error', error);
+        //this.displayErrorPostingReview = true;
+      });
+  }
+
+  private getUsageRate(rhId: string) {
+    this.coachCoacheeService.getUsageRate(rhId).subscribe(
+      (rate: RhUsageRate) => {
+        console.log("getUsageRate, rate : ", rate);
+        this.coacheeUsageRate = Observable.of(rate);
+      }
+    );
+  }
+
+
+
   updateCoacheeObjectivePanelVisibility(visible: boolean) {
     if (visible) {
-      $('#add_potential_coachee_modal').openModal();
+      $('#modify_coachee_objective_modal').openModal();
     } else {
-      $('#add_potential_coachee_modal').closeModal();
+      $('#modify_coachee_objective_modal').closeModal();
     }
   }
 
@@ -113,10 +182,15 @@ export class MeetingItemRhComponent implements OnInit, AfterViewInit {
         console.log('addObjectiveToCoachee, SUCCESS', obj);
         // close modal
         this.updateCoacheeObjectivePanelVisibility(false);
+        this.objectiveChanged.emit(this.coacheeNewObjective);
+        Materialize.toast("L'objectif a été modifié !", 3000, 'rounded')
         // TODO stop loader
-
         // clean
         this.coacheeNewObjective = null;
+      }, (error) => {
+        console.log('addObjectiveToCoachee, error', error);
+
+        Materialize.toast("Imposible de modifier l'objectif", 3000, 'rounded')
       }
     );
   }
