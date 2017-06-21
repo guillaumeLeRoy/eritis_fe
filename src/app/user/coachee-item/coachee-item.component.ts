6bec1e32b05db11cb8024db5ca34b0f534916751
jordhan <jordhan.madec@gmail.com>
2017-06-21 17:12:26 +0200

    Coachee list for admin

---
+++ src/app/user/coachee-item/coachee-item.component.ts
@@ -0,0 +1,46 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Coachee} from "../../model/Coachee";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'rb-coachee-item',
+  templateUrl: './coachee-item.component.html',
+  styleUrls: ['./coachee-item.component.css']
+})
+export class CoacheeItemComponent implements OnInit {
+
+  @Input()
+  coachee: Coachee;
+
+  months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  goToCoacheeProfile(coacheeId: String) {
+    window.scrollTo(0, 0);
+    this.router.navigate(['/profile_coachee', 'visiter', coacheeId]);
+  }
+
+  printDateString(date: string) {
+    return this.getDate(date);
+  }
+
+  getHours(date: string) {
+    return (new Date(date)).getHours();
+  }
+
+  getMinutes(date: string) {
+    let m = (new Date(date)).getMinutes();
+    if (m === 0)
+      return '00';
+    return m;
+  }
+
+  getDate(date: string): string {
+    return (new Date(date)).getDate() + ' ' + this.months[(new Date(date)).getMonth()] + ' ' + (new Date(date)).getFullYear();
+  }
+
+}
---
+++ src/app/user/rh-item/rh-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'er-rh-item',
+  templateUrl: './rh-item.component.html',
+  styleUrls: ['./rh-item.component.css']
+})
+export class RhItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
