a61b4b497e00b6fbfee3c41da651c2e49f32206e
guillaume <gleroy78@gmail.com>
2017-07-18 14:22:03 +0100

    fix typo again ...

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -640,7 +640,7 @@ export class AuthService {
     console.log("updateCoacheeForId, id", id);
 
     let body = {
-      first_name: first_name,
+      first_name: firstName,
       last_name: last_name,
       avatar_url: avatarUrl,
     };
