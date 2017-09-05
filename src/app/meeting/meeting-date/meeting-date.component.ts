ef9f1c59014f8ea5616df7ef3f4907b62332662b
jordhan <jordhan.madec@gmail.com>
2017-09-05 11:28:27 +0200

    Datepicker in french

--- src/app/meeting/meeting-date/meeting-date.component.ts
+++ src/app/meeting/meeting-date/meeting-date.component.ts
@@ -1,5 +1,5 @@
-import {ChangeDetectorRef, Component, OnDestroy, OnInit} from "@angular/core";
-import {NgbDateStruct} from "@ng-bootstrap/ng-bootstrap";
+import {ChangeDetectorRef, Component, Injectable, OnDestroy, OnInit} from "@angular/core";
+import {NgbDatepickerI18n, NgbDateStruct} from "@ng-bootstrap/ng-bootstrap";
 import {ApiUser} from "../../model/ApiUser";
 import {AuthService} from "../../service/auth.service";
 import {Observable, Subscription} from "rxjs";
@@ -11,10 +11,45 @@ import {Utils} from "../../utils/Utils";
 
 declare var Materialize: any;
 
+const I18N_VALUES = {
+  'fr': {
+    weekdays: ['Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa', 'Di'],
+    months: Utils.months
+  }
+  // other languages you would support
+};
+
+// Define a service holding the language. You probably already have one if your app is i18ned. Or you could also
+// use the Angular LOCALE_ID value
+@Injectable()
+export class I18n {
+  language = 'fr';
+}
+
+// Define custom service providing the months and weekdays translations
+@Injectable()
+export class CustomDatepickerI18n extends NgbDatepickerI18n {
+
+  constructor(private _i18n: I18n) {
+    super();
+  }
+
+  getWeekdayShortName(weekday: number): string {
+    return I18N_VALUES[this._i18n.language].weekdays[weekday - 1];
+  }
+  getMonthShortName(month: number): string {
+    return I18N_VALUES[this._i18n.language].months[month - 1];
+  }
+  getMonthFullName(month: number): string {
+    return this.getMonthShortName(month);
+  }
+}
+
 @Component({
   selector: 'rb-meeting-date',
   templateUrl: './meeting-date.component.html',
-  styleUrls: ['./meeting-date.component.scss']
+  styleUrls: ['./meeting-date.component.scss'],
+  providers: [I18n, {provide: NgbDatepickerI18n, useClass: CustomDatepickerI18n}] // define custom NgbDatepickerI18n provider
 })
 export class MeetingDateComponent implements OnInit, OnDestroy {
 
