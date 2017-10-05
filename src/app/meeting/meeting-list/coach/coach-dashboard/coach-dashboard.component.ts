e2e7a01869e1712c16812469079223246ac4817f
Guillaume Le Roy <guillaume.leroy@ratedpeople.com>
2017-10-05 18:11:37 +0100

    continue

--- src/app/meeting/meeting-list/coach/coach-dashboard/coach-dashboard.component.ts
+++ src/app/meeting/meeting-list/coach/coach-dashboard/coach-dashboard.component.ts
@@ -6,6 +6,9 @@ import {Coach} from "../../../../model/Coach";
 import {HR} from "../../../../model/HR";
 import {Coachee} from "../../../../model/Coachee";
 import {BehaviorSubject} from "rxjs/BehaviorSubject";
+import {Meeting} from "../../../../model/Meeting";
+import {Observable} from "rxjs/Observable";
+import {MeetingsService} from "../../../../service/meetings.service";
 
 declare var $: any;
 declare var Materialize: any;
@@ -20,8 +23,13 @@ export class CoachDashboardComponent implements OnInit, AfterViewInit, OnDestroy
   private user: BehaviorSubject<ApiUser>;
 
   private connectedUserSubscription: Subscription;
+  private getAllMeetingsForCoachIdSubscription: Subscription;
 
-  constructor(private authService: AuthService) {
+  private meetingsArray: Array<Meeting>;
+
+  private meetingsOpened: Observable<Array<Meeting>>;
+
+  constructor(private authService: AuthService, private meetingsService: MeetingsService) {
     this.user = new BehaviorSubject(null);
   }
 
@@ -37,6 +45,10 @@ export class CoachDashboardComponent implements OnInit, AfterViewInit, OnDestroy
   ngOnDestroy(): void {
     if (this.connectedUserSubscription) {
       this.connectedUserSubscription.unsubscribe();
+    }
+
+    if (this.getAllMeetingsForCoachIdSubscription) {
+      this.getAllMeetingsForCoachIdSubscription.unsubscribe();
     }
   }
 
@@ -58,6 +70,42 @@ export class CoachDashboardComponent implements OnInit, AfterViewInit, OnDestroy
       }
 
       this.user.next(user);
+      this.getAllMeetingsForCoach(user.id);
+    }
+  }
+
+  private getAllMeetingsForCoach(coachId: string) {
+    this.getAllMeetingsForCoachIdSubscription = this.meetingsService.getAllMeetingsForCoachId(coachId, false)
+      .subscribe(
+        (meetings: Meeting[]) => {
+          console.log('got meetings for coach', meetings);
+          this.onMeetingsObtained(meetings);
+        }, (error) => {
+          console.log('got meetings for coach ERROR', error);
+        }
+      );
+  }
+
+  private onMeetingsObtained(meetings: Array<Meeting>) {
+    console.log('got meetings for coach', meetings);
+
+    this.meetingsArray = meetings;
+    this.getBookedMeetings();
+  }
+
+  private getBookedMeetings() {
+    console.log('getBookedMeetings');
+    if (this.meetingsArray != null) {
+      let opened: Array<Meeting> = new Array<Meeting>();
+      for (let meeting of this.meetingsArray) {
+        console.log('getBookedMeetings, meeting : ', meeting);
+
+        if (meeting != null && meeting.isOpen && meeting.agreed_date != undefined) {
+          opened.push(meeting);
+          console.log('getBookedMeetings, add meeting');
+        }
+      }
+      this.meetingsOpened = Observable.of(opened);
     }
   }
 
